062a0f859653c373a9d20a6f51c84c000daaf7eb
rika <rika.okoshi@gmail.com>
2022-06-13 16:03:03 +0900

    Handle the message reserve NFT transfer

--- x/nti/keeper/msg_server_reserve_nft_transfer.go
+++ x/nti/keeper/msg_server_reserve_nft_transfer.go
@@ -2,16 +2,50 @@ package keeper
 
 import (
 	"context"
+	"crypto/sha256"
+	"encoding/hex"
+	"strconv"
+	"strings"
+	"time"
+
+	"nti/x/nti/types"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"nti/x/nti/types"
 )
 
 func (k msgServer) ReserveNftTransfer(goCtx context.Context, msg *types.MsgReserveNftTransfer) (*types.MsgReserveNftTransferResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	// Generate reserved key.
+	elems := []string{
+		msg.SrcNftHash,
+		msg.SrcChain,
+		msg.SrcAddr,
+		msg.DestNftHash,
+		msg.DestChain,
+		msg.DestAddr,
+		// Convert block height into string.
+		strconv.FormatInt(int64(msg.BlockHeight), 10),
+	}
+	joined := strings.Join(elems, ",")
+	sum := sha256.Sum256([]byte(joined))
+	reservedKey := hex.EncodeToString(sum[:])
+
+	createdAt := time.Now().Unix()
+
+	reservedNftTransfer := types.ReservedNftTransfer{
+		ReservedKey: reservedKey,
+		SrcNftHash:  msg.SrcNftHash,
+		SrcChain:    msg.SrcChain,
+		SrcAddr:     msg.SrcAddr,
+		DestNftHash: msg.DestNftHash,
+		DestChain:   msg.DestChain,
+		DestAddr:    msg.DestAddr,
+		BlockHeight: msg.BlockHeight,
+		CreatedAt:   int32(int(createdAt)),
+	}
+
+	k.Keeper.SetReservedNftTransfer(ctx, reservedNftTransfer)
 
 	return &types.MsgReserveNftTransferResponse{}, nil
 }
