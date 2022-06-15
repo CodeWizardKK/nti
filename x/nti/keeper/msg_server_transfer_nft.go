881e033e4824c2e231a16e5894f5410984ea3a8b
rika <rika.okoshi@gmail.com>
2022-06-15 13:09:09 +0900

    Handle the message transfer NFT

--- x/nti/keeper/msg_server_transfer_nft.go
+++ x/nti/keeper/msg_server_transfer_nft.go
@@ -2,16 +2,33 @@ package keeper
 
 import (
 	"context"
+	"strconv"
+	"time"
+
+	"nti/x/nti/types"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"nti/x/nti/types"
 )
 
 func (k msgServer) TransferNft(goCtx context.Context, msg *types.MsgTransferNft) (*types.MsgTransferNftResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
+	index := len(k.Keeper.GetAllNftTransfer(ctx))
+	createdAt := time.Now().Unix()
+
 	// TODO: Handling the message
-	_ = ctx
+	nftTransfer := types.NftTransfer{
+		Index:       strconv.Itoa(index),
+		SrcNftHash:  msg.SrcNftHash,
+		SrcChain:    msg.SrcChain,
+		SrcAddr:     msg.SrcAddr,
+		DestNftHash: msg.DestNftHash,
+		DestChain:   msg.DestChain,
+		DestAddr:    msg.DestAddr,
+		CreatedAt:   int32(int(createdAt)),
+	}
+
+	k.Keeper.SetNftTransfer(ctx, nftTransfer)
 
 	return &types.MsgTransferNftResponse{}, nil
 }
