87c8abfdadca0253c0502ee2ab78da7b6c24c6ee
rika <rika.okoshi@gmail.com>
2022-07-20 09:26:00 +0900

    Get keplr accounts

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -11,8 +11,9 @@
 </template>
 
 <script lang="ts">
+import { SigningCosmosClient } from '@cosmjs/launchpad'
 import { SpNavbar, SpTheme } from '@starport/vue'
-import { computed, onBeforeMount } from 'vue'
+import { computed, onBeforeMount, onMounted } from 'vue'
 import { useRouter } from 'vue-router'
 import { useStore } from 'vuex'
 
@@ -43,6 +44,38 @@ export default {
       await $s.dispatch("nti.nti/QueryNftTransferAll",{options:{subscribe:true, all:true},params:{}})
 
       router.push('portfolio')
+    })
+
+    onMounted(async () => {
+      if (!window.keplr) {
+          alert("Please install keplr extension");
+      } else {
+          console.log(window.keplr)
+          const chainId = "cosmoshub-4";
+
+          // Enabling before using the Keplr is recommended.
+          // This method will ask the user whether to allow access if they haven't visited this website.
+          // Also, it will request that the user unlock the wallet if the wallet is locked.
+          await window.keplr.enable(chainId);
+      
+          const offlineSigner = window.keplr.getOfflineSigner(chainId);
+      
+          // You can get the address/public keys by `getAccounts` method.
+          // It can return the array of address/public key.
+          // But, currently, Keplr extension manages only one address/public key pair.
+          // XXX: This line is needed to set the sender address for SigningCosmosClient.
+          const accounts = await offlineSigner.getAccounts();
+          console.log(accounts)
+      
+          // Initialize the gaia api with the offline signer that is injected by Keplr extension.
+          const cosmJS = new SigningCosmosClient(
+              "https://lcd-cosmoshub.keplr.app",
+              accounts[0].address,
+              offlineSigner,
+          );
+
+          console.log(cosmJS)
+      }
     })
 
     return {
