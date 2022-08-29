40ce64a9f1548f54069023dc01f2335f17cd794c
rika <rika.okoshi@gmail.com>
2022-08-29 17:11:24 +0900

    Fix

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,7 +1,6 @@
 import starportLibrary from '@starport/vue'
 import { createApp } from 'vue'
 import Antd from 'ant-design-vue';
-import App from './App';
 import 'ant-design-vue/dist/antd.css';
 
 import App from './App.vue'
