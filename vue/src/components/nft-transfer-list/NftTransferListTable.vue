077183aaa39369eaee03f19241f41ad5eb923926
rika <rika.okoshi@gmail.com>
2022-08-29 16:36:26 +0900

    Add script to NFT transfer list view

---
+++ vue/src/components/nft-transfer-list/NftTransferListTable.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <table>
+        <thead>
+            <tr>
+                <th v-for="(key, index) in headers" :key="index">
+                    {{ key }}
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="(item, itemIndex) in items" :key="itemIndex">
+                <td v-for="(key, columnIndex) in columns" :key="columnIndex">
+                    <div>
+                        {{ item[key] }}
+                    </div>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+const headers = [
+    'Reserved Key',
+    'NFT Source Hash',
+    'NFT Source Chain',
+    'NFT Source Address',
+    'NFT Destination Chain',
+    'NFT Destination Address',
+    'Fungible Token',
+    'FT Chain',
+    'FT Source Address',
+    'FT Destination Address',
+    'Block Height',
+    'Created At'
+]
+
+const columns = [
+    "reservedKey",
+    "nftSrcHash",
+    "nftSrcChain",
+    "nftSrcAddr",
+    "nftDestChain",
+    "nftDestAddr",
+    "fungibleToken",
+    "ftChain",
+    "ftSrcAddr",
+    "ftDestAddr",
+    "blockHeight",
+    "createdAt"
+]
+
+export default {
+    props: {
+        items: Object
+    },
+    setup(props) {
+        const items = props.items
+
+        return {
+            headers,
+            columns,
+            items,
+        }
+    }
+};
+</script>
--- vue/src/views/NftTransferList.vue
+++ vue/src/views/NftTransferList.vue
@@ -1,3 +1,36 @@
 <template>
-    <div>NFT Transfer List</div>
+  <div>
+    <h3>NFT Transfer List</h3>
+    <nft-transfer-list-table
+    :items="items"></nft-transfer-list-table>
+  </div>
 </template>
+
+<script>
+import { computed } from 'vue'
+import { useStore } from 'vuex'
+import NftTransferListTable from '../components/nft-transfer-list/NftTransferListTable.vue';
+
+export default {
+    components: {
+        NftTransferListTable
+    },
+    setup() {
+        // store
+        let $s = useStore()
+
+        // computed
+        const items = computed(() => {
+            return (
+                $s.getters["nti.nti/getReservedNftTransferAll"]({
+                    params: {}
+                })?.reservedNftTransfer ?? []
+            )
+        })
+
+        return {
+            items,
+        }
+    }
+};
+</script>
