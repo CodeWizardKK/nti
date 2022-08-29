04536e407e15a616eda404d6ef7b6e739c465bbe
rika <rika.okoshi@gmail.com>
2022-08-29 17:07:23 +0900

    Register all Antd components

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,9 +1,12 @@
 import starportLibrary from '@starport/vue'
 import { createApp } from 'vue'
+import Antd from 'ant-design-vue';
+import App from './App';
+import 'ant-design-vue/dist/antd.css';
 
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
 const app = createApp(App)
-app.use(store).use(router).use(starportLibrary).mount('#app')
+app.use(store).use(router).use(starportLibrary).use(Antd).mount('#app')
