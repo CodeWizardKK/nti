e8809bb8dbbbecdd4856af29431978d331270467
rika <rika.okoshi@gmail.com>
2022-08-30 13:07:19 +0900

    Try ant design form

--- vue/src/components/nft-transfer-edit/NftTransferEditForm.vue
+++ vue/src/components/nft-transfer-edit/NftTransferEditForm.vue
@@ -1,80 +1,62 @@
-
 <template>
-    <div>
-        <div class="sp-papers__main sp-box sp-shadow sp-form-group">
-            <form class="sp-papers__main__form">
-            <div class="sp-papers__main__rcpt__header sp-box-header">
-                Reserve NFT Transfer
-            </div>
-
-            <input class="sp-input" placeholder="NFT Source Hash" v-model="nftSrcHash" />
-            <input class="sp-input" placeholder="NFT Source Chain" v-model="nftSrcChain" />
-            <input class="sp-input" placeholder="NFT Source Address" v-model="nftSrcAddr" />
-            <input class="sp-input" placeholder="NFT Destination Chain" v-model="nftDestChain" />
-            <input class="sp-input" placeholder="NFT Destination Address" v-model="nftDestAddr" />
-            <input class="sp-input" placeholder="Fungible Token" v-model="fungibleToken" />
-            <input class="sp-input" placeholder="FT Chain" v-model="ftChain" />
-            <input class="sp-input" placeholder="FT Source Address" v-model="ftSrcAddr" />
-            <input class="sp-input" placeholder="FT Destination Address" v-model="ftDestAddr" />
-            <input class="sp-input" placeholder="Block Height" v-model="blockHeight" />
-            <sp-button @click="transferNft">Reserve NFT Transfer</sp-button>
-            </form>
-        </div>
-    </div>
+    <a-form
+    :model="formState"
+    name="transfer"
+    :label-col="{ span: 8 }"
+    :wrapper-col="{ span: 16 }"
+    autocomplete="off"
+    @finish="onFinish"
+    @finishFailed="onFinishFailed">
+    {{ formState }}
+        <a-form-item label="Token ID" name="tokenId" :rules="[{ required: true, message: 'Please input Token ID!' }]">
+            <a-input :value="formState.tokenId"></a-input>
+        </a-form-item>
+        <a-form-item :wrapper-col="{ offset: 8, span: 16 }">
+            <a-button type="primary" html-type="submit">Transfer</a-button>
+        </a-form-item>
+    </a-form>
 </template>
-<script>
-import { ref } from 'vue'
+<script lang="ts">
+import { reactive } from 'vue'
 import useAccount from '../../composables/useAccount'
+
+interface FormState {
+    tokenId: string;
+}
 
 export default {
   name: "Reserve NFT Transer Form",
   emits: ['reserveNftTransfer'],
 
-  setup(props, context) {
+  setup(props: any, context: any) {
     const { currentAccount } = useAccount()
 
     // state
-    const nftSrcHash = ref('')
-    const nftSrcChain = ref('')
-    const nftSrcAddr = ref('')
-    const nftDestChain = ref('')
-    const nftDestAddr = ref('')
-    const ftChain = ref('')
-    const ftSrcAddr = ref('')
-    const ftDestAddr = ref('')
-    const fungibleToken = ref('')
-    const blockHeight = ref('')
+    const formState = reactive<FormState>({
+        tokenId: '',
+    })
 
     // methods
+    const onFinish = (values: any) => {
+        console.log('Success:', values)
+    }
+
+    const onFinishFailed = (errorInfo: any) => {
+        console.log('Failed:', errorInfo)
+    }
+
     const transferNft = () => {
         const value = {
             creator: currentAccount.value,
-            nftSrcHash: nftSrcHash.value,
-            nftSrcChain: nftSrcChain.value,
-            nftSrcAddr: nftSrcAddr.value,
-            nftDestChain: nftDestChain.value,
-            nftDestAddr: nftDestAddr.value,
-            ftChain: ftChain.value,
-            ftSrcAddr: ftSrcAddr.value,
-            ftDestAddr: ftDestAddr.value,
-            fungibleToken: fungibleToken.value,
-            blockHeight: blockHeight.value,
         };
 
         context.emit('reserveNftTransfer', value)
     }
 
     return {
-        nftSrcHash,
-        nftSrcChain,
-        nftSrcAddr,
-        nftDestChain,
-        nftDestAddr,
-        ftChain,
-        ftSrcAddr,
-        ftDestAddr,
-        fungibleToken,
-        blockHeight,
+        formState,
+        onFinish,
+        onFinishFailed,
         transferNft,
     }
   },
