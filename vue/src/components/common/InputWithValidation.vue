500279c45488fe47dba20c2adee11c230285f449
rika <rika.okoshi@gmail.com>
2022-09-07 09:43:42 +0900

    Try InputWithValidation component

---
+++ vue/src/components/common/InputWithValidation.vue
@@ -0,0 +1,36 @@
+<template>
+  <a-form-item
+    :label="label"
+    :has-feedback="!!errorMessage"
+    :help="errorMessage"
+    :validate-status="errorMessage ? 'error' : undefined"
+  >
+    <a-input :value="value" :type="type" />
+    {{ value }}
+  </a-form-item>
+</template>
+
+<script setup lang="ts">
+import { toRef } from 'vue';
+import { useField } from 'vee-validate';
+
+const props = defineProps({
+    name: {
+        type: String,
+        required: true,
+    },
+    label: {
+        type: String,
+        required: true,
+    },
+    type: {
+        type: String,
+        required: false,
+    },
+})
+
+const { value, errorMessage } = useField(toRef(props, 'name'));
+console.log(props)
+console.log(useField(toRef(props, 'name')))
+console.log(value)
+</script>
--- vue/src/components/nft-transfer-edit/NftTransferEditForm.vue
+++ vue/src/components/nft-transfer-edit/NftTransferEditForm.vue
@@ -2,16 +2,16 @@
   <Form :validation-schema="schema" @submit="onSubmit">
     <!-- You can use the field component to wrap a q-* component -->
     <!-- Do this if you have only one or a few places that need validation -->
-    <Field name="tokenId" v-slot="{ value, handleChange, errorMessage }">
-      <a-form-item
-        label="Token ID"
-        :has-feedback="!!errorMessage"
-        :help="errorMessage"
-        :validate-status="errorMessage ? 'error' : undefined"
-      >
-        <a-input :value="value" @update:value="handleChange" />
-      </a-form-item>
-    </Field>
+    <input-with-validation name="nftTokenId" label="Token ID"></input-with-validation>
+    <input-with-validation name="nftSrcChain" label="Blockchain"></input-with-validation>
+    <input-with-validation name="nftSrcAddr" label="Address"></input-with-validation>
+    <input-with-validation name="nftDestChain" label="Blockchain"></input-with-validation>
+    <input-with-validation name="nftDestAddr" label="Address"></input-with-validation>
+    <input-with-validation name="fungibleToken" label="Amount"></input-with-validation>
+    <input-with-validation name="ftChain" label="Blockchain"></input-with-validation>
+    <input-with-validation name="ftSrcAddr" label="Source address"></input-with-validation>
+    <input-with-validation name="ftDestAddr" label="Destination address"></input-with-validation>
+    <input-with-validation name="blockHeight" label="Block height"></input-with-validation>
 
     <a-form-item>
       <a-button type="primary" html-type="submit">Submit</a-button>
@@ -19,39 +19,33 @@
   </Form>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import { Field, Form } from 'vee-validate';
 import * as yup from 'yup';
 import useAccount from '../../composables/useAccount';
+import InputWithValidation from '../common/InputWithValidation.vue';
 
-export default {
-    emits: ['reserveNftTransfer'],
-    components: { Field, Form },
+const schema = yup.object({
+    nftTokenId: yup.string().required().label('Token ID'),
+    nftSrcChain: yup.string().required().label('Blockchain'),
+    nftSrcAddr: yup.string().required().label('Address'),
+    nftDestChain: yup.string().required().label('Blockchain'),
+    nftDestAddr: yup.string().required().label('Address'),
+    fungibleToken: yup.string().required().label('Amount'),
+    ftChain: yup.string().required().label('Blockchain'),
+    ftSrcAddr: yup.string().required().label('Source address'),
+    ftDestAddr: yup.string().required().label('Destination address'),
+    blockHeight: yup.string().required().label('Block height'),
+});
 
-    setup(props: any, context: any) {
-        const { currentAccount } = useAccount()
+const onSubmit = (values: any) => {
+    console.log('Success:', values)
+    // const value = {
+        // creator: currentAccount.value,
+    // };
 
-        const schema = yup.object({
-            tokenId: yup.string().required().label('Token ID'),
-            nftSrcAddr: yup.string().required().label('Address'),
-        });
-
-        const onSubmit = (values: any) => {
-            console.log('Success:', values)
-            const value = {
-                creator: currentAccount.value,
-            };
-
-            context.emit('reserveNftTransfer', value)
-        }
-
-        return {
-            onSubmit,
-            schema,
-        }
-    },
+    // context.emit('reserveNftTransfer', value)
 }
-
 
 </script>
 
