823a54862290eb308c20d3b898a24374f09132ae
rika <rika.okoshi@gmail.com>
2022-10-27 14:40:03 +0900

    Add a script

---
+++ scripts/check_nft_transfer_status.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+
+	"google.golang.org/grpc"
+
+	"github.com/cosmos/cosmos-sdk/codec"
+)
+
+func checkReservedList() error {
+	// Create a connection to the gRPC server.
+	grpcConn, err := grpc.Dial(
+		"127.0.0.1:9090",    // your gRPC server address.
+		grpc.WithInsecure(), // The Cosmos SDK doesn't support any transport security mechanism.
+		// This instantiates a general gRPC codec which handles proto bytes. We pass in a nil interface registry
+		// if the request/response types contain interface instead of 'nil' you should pass the application specific codec.
+		grpc.WithDefaultCallOptions(grpc.ForceCodec(codec.NewProtoCodec(nil).GRPCCodec())),
+	)
+	if err != nil {
+		return err
+	}
+	defer grpcConn.Close()
+
+	return nil
+}
+
+func main() {
+	err := checkReservedList()
+	if err != nil {
+		fmt.Println(err)
+	}
+}
