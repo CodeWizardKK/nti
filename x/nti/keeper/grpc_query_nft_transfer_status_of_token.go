c3d930d8b48f4311f281d2f0fa393f39cd9f85a3
rika <rika.okoshi@gmail.com>
2022-12-22 17:45:33 +0900

    Handle query NftTransferStatusOfToken

--- x/nti/keeper/grpc_query_nft_transfer_status_of_token.go
+++ x/nti/keeper/grpc_query_nft_transfer_status_of_token.go
@@ -2,6 +2,7 @@ package keeper
 
 import (
 	"context"
+	"reflect"
 
 	"nti/x/nti/types"
 
@@ -10,6 +11,33 @@ import (
 	"google.golang.org/grpc/status"
 )
 
+func getTransferStatus(reservedKey string, nftTransferStatus types.NftTransferStatus) TransferStatus {
+	var transferStatus TransferStatus
+	nftTransferStatusValue := reflect.ValueOf(&nftTransferStatus)
+
+	// 各ステータスのリストから検索
+	for ts := 0; ts < int(Completed); ts++ {
+		targetStatus := TransferStatus(ts).String()
+		reservedKeyList := nftTransferStatusValue.FieldByName(targetStatus)
+
+		found := false
+		len := reservedKeyList.Len()
+		for i := 0; i < len; i++ {
+			if reservedKeyList.Index(i).String() == reservedKey {
+				found = true
+				break
+			}
+		}
+
+		if found {
+			transferStatus = TransferStatus(ts)
+			break
+		}
+	}
+
+	return transferStatus
+}
+
 func (k Keeper) NftTransferStatusOfToken(goCtx context.Context, req *types.QueryNftTransferStatusOfTokenRequest) (*types.QueryNftTransferStatusOfTokenResponse, error) {
 	if req == nil {
 		return nil, status.Error(codes.InvalidArgument, "invalid request")
@@ -17,13 +45,38 @@ func (k Keeper) NftTransferStatusOfToken(goCtx context.Context, req *types.Query
 
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Process the query
+	// TODO: pagination
+	// 検索条件に該当する移転予約のステータスリスト
 	var nftTransferStatusList []types.NftTransferStatusDetail
-	_ = ctx
 
-	// reservedKey := ""
+	// 全予約を取得
+	reservedNftTransferList := k.GetAllReservedNftTransfer(ctx)
+	// ステータスリストを取得
+	nftTransferStatus, _ := k.GetNftTransferStatus(ctx)
 
-	// status := ""
+	for _, reservedNftTransfer := range reservedNftTransferList {
+		// TODO: Check req.Chain, req.ContractAddr
+		if reservedNftTransfer.NftTokenId == req.TokenId {
+			reservedKey := reservedNftTransfer.ReservedKey
+
+			transferStatus := getTransferStatus(reservedKey, nftTransferStatus)
+
+			// トランザクションハッシュを取得
+			transactionHash := ""
+			if transferStatus >= Waiting {
+				nftMint, _ := k.GetNftMint(ctx, reservedKey)
+				transactionHash = nftMint.TransactionHash
+			}
+
+			nftTransferStatusDetail := types.NftTransferStatusDetail{
+				ReservedKey:     reservedKey,
+				ReservedData:    &reservedNftTransfer,
+				TransferStatus:  int32(transferStatus),
+				TransactionHash: transactionHash,
+			}
+			nftTransferStatusList = append(nftTransferStatusList, nftTransferStatusDetail)
+		}
+	}
 
 	return &types.QueryNftTransferStatusOfTokenResponse{
 		NftTransferStatusDetail: nftTransferStatusList,
